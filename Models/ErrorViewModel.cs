09820a689cdf42d9277712a9fd6c6a52d3e9bbb9
Eirene Karunungan <eireneysabelkarunungan@gmail.com>
2026-09-11 21:20:52 +0800

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace IT_ELECTIVE_2_PREFINAL_Karunungan_Eirene.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
