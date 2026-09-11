cb7e0df9496e6cc9eec5e8053d8a4e6e5c1c1211
Eirene Karunungan <eireneysabelkarunungan@gmail.com>
2026-09-11 21:57:45 +0800

    added models, controllers, and views

---
+++ Controllers/QuestionController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using IT_ELECTIVE_2_PREFINAL_Karunungan_Eirene.Models;
+
+namespace IT_ELECTIVE_2_PREFINAL_Karunungan_Eirene.Controllers
+{
+    public class QuestionsController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View(QuestionData.All.OrderBy(q => q.Number).ToList());
+
+
+        }
+
+        public IActionResult Details(int id)
+        {
+            var question = QuestionData.All.FirstOrDefault(q => q.Number == id);
+
+            if (question == null)
+            {
+
+
+                return NotFound();
+            }
+
+            return View(question);
+
+            //return View();
+            //return NotFound;
+        }
+    }
+}
